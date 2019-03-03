f971d1f948ecbba5eb506f5b0fd719a577b3b316
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-03-03 16:08:15 +0800

    Init

---
+++ aspnet-core/src/DgERM.Core/DgERMConsts.cs
@@ -0,0 +1,11 @@
+﻿namespace DgERM
+{
+    public class DgERMConsts
+    {
+        public const string LocalizationSourceName = "DgERM";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+    }
+}
---
+++ aspnet-core/src/DgERM.EntityFrameworkCore/EntityFrameworkCore/DgERMDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using DgERM.Authorization.Roles;
+using DgERM.Authorization.Users;
+using DgERM.MultiTenancy;
+
+namespace DgERM.EntityFrameworkCore
+{
+    public class DgERMDbContext : AbpZeroDbContext<Tenant, Role, User, DgERMDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public DgERMDbContext(DbContextOptions<DgERMDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
