3a638236189364f14710bdd143d569c4495fcb8b
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-03-16 21:15:03 +0800

    Update PermissionNames.cs

--- aspnet-core/src/DgERM.Core/Authorization/PermissionNames.cs
+++ aspnet-core/src/DgERM.Core/Authorization/PermissionNames.cs
@@ -7,5 +7,13 @@
         public const string Pages_Users = "Pages.Users";
 
         public const string Pages_Roles = "Pages.Roles";
+
+
+        public const string Pages = "Pages";
+        public const string Pages_Tasks = "Pages.Pages.Tasks";
+
+        public const string Pages_Tasks_AssignPerson = "Pages.Pages.Tasks.AssignPerson";
+
+        public const string Pages_Tasks_Delete = "Pages.Pages.Tasks.Delete";
     }
 }
