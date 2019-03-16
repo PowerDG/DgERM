ba0c5ad733134abb123908faef8a84c67a312fde
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-03-16 21:15:00 +0800

    Update IDtoMapping.cs

--- aspnet-core/src/DgERM.Application/IDtoMapping.cs
+++ aspnet-core/src/DgERM.Application/IDtoMapping.cs
@@ -8,6 +8,10 @@ namespace DgERM
     /// <summary>
     ///     实现该接口以进行映射规则创建
     /// </summary>
+//    /// 
+//    定义抽象接口IDtoMapping
+
+//应用服务层根目录创建IDtoMapping接口，定义CreateMapping方法由映射规则类实现。
     internal interface IDtoMapping
     {
         void CreateMapping(IMapperConfigurationExpression mapperConfig);
