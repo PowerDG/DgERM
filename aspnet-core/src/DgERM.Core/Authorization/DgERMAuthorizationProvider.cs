51719416177f586111e4476b4a6f7d5dd6f542bb
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-03-16 21:14:57 +0800

    Update DgERMAuthorizationProvider.cs

--- aspnet-core/src/DgERM.Core/Authorization/DgERMAuthorizationProvider.cs
+++ aspnet-core/src/DgERM.Core/Authorization/DgERMAuthorizationProvider.cs
@@ -11,6 +11,15 @@ namespace DgERM.Authorization
             context.CreatePermission(PermissionNames.Pages_Users, L("Users"));
             context.CreatePermission(PermissionNames.Pages_Roles, L("Roles"));
             context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"), multiTenancySides: MultiTenancySides.Host);
+
+            var pages = context.GetPermissionOrNull(PermissionNames.Pages);
+            if (pages == null)
+                pages = context.CreatePermission(PermissionNames.Pages, L("Pages"));
+            //Tasks
+            var tasks = pages.CreateChildPermission(PermissionNames.Pages_Tasks, L("Tasks"));
+            tasks.CreateChildPermission(PermissionNames.Pages_Tasks_AssignPerson, L("AssignTaskToPerson"));
+            tasks.CreateChildPermission(PermissionNames.Pages_Tasks_Delete, L("DeleteTask"));
+
         }
 
         private static ILocalizableString L(string name)
