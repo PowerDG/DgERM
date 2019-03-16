3bf12b283a7be8dcec49da8a672d84fc37dca9e1
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-03-16 21:15:40 +0800

    Update TaskDto.cs

--- aspnet-core/src/DgERM.Application/DgDemo/Tasks/TaskDto.cs
+++ aspnet-core/src/DgERM.Application/DgDemo/Tasks/TaskDto.cs
@@ -13,6 +13,35 @@ namespace DgERM.DgDemo.Tasks
     /// A DTO class that can be used in various application service methods when needed to send/receive Task objects.
     /// </summary>
     /// 
+
+
+//        使用AutoMapper自动映射DTO与实体
+//4.1. 简要介绍AutoMapper
+//开始之前，如果对AutoMapper不是很了解，
+//建议看下这篇文章AutoMapper小结。
+//    http://www.cnblogs.com/jobs2/p/3503990.html
+//AutoMapper的使用步骤，简单总结下：
+
+
+//创建映射规则（Mapper.CreateMap<source, destination>();）
+//类型映射转换（Mapper.Map<source, destination>(sourceModel)）
+
+//在Abp中有两种方式创建映射规则：
+
+//特性数据注解方式：
+
+//AutoMapFrom、AutoMapTo 特性创建单向映射
+//AutoMap 特性创建双向映射
+
+
+//代码创建映射规则：
+
+//Mapper.CreateMap<source, destination>();
+
+//作者：圣杰
+//链接：https://www.jianshu.com/p/da69ca7b27c6
+//来源：简书
+//简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
     [AutoMapTo(typeof(Task))] //定义单向映射    
     public class TaskDto : EntityDto
     {
@@ -114,5 +143,7 @@ namespace DgERM.DgDemo.Tasks
         public TaskState? State { get; set; }
 
         public int? AssignedPersonId { get; set; }
+        public int? Sorting { get;   set; }
+        public string Filter { get;   set; }
     }
 }
